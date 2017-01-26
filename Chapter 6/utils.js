06c57c726533f9dbba8cc8d54f887eca064eafa4
4minute <yeting9177@126.com>
2017-01-26 14:08:05 +0800

    chapter 6

---
+++ Chapter 6/utils.js
@@ -0,0 +1,77 @@
+var utils={}
+utils.colorToRGB = function (color,alpha){
+	if(typeof color === 'string' && color[0] ==='#'){
+		color = window.parseInt(color.slice(1),16);
+	}
+	alpha = (alpha === undefined)?1:alpha;
+
+	var r = color >> 16 & 0xff,
+		g = color >> 8 & 0xff,
+		b = color & 0xff,
+		a = (alpha < 0)?0:((alpha > 1)?1:alpha);
+
+	if(a===1){
+		return "rgb("+r+","+g+","+b+")";
+	}else{
+		return "rgba("+r+","+g+","+b+","+a+")";
+	}
+};
+
+utils.parseColor = function(color,toNumber){
+	if(toNumber === true){
+		if(typeof color === 'number'){
+			return (color|0);
+		}
+		if(typeof color === 'string' && color[0] === '#'){
+			color = color.slice(1);
+		}
+		return window.parseInt(color,16);
+	}else{
+		if(typeof color === 'number'){
+			color = '#' + ('00000' + (color|0).toString(16)).substr(-6);
+		}
+		return color;
+	}
+};
+
+
+utils.captureMouse=function(element){
+	var mouse = {x:0,y:0};
+
+	element.addEventListener('mousemove',function(event){
+		var x,y;
+		if(event.pageX||event.pageY){
+			x = event.pageX;
+			y = event.pageY;
+		}else{
+			x = event.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
+			y = event.clientY + document.body.scrollTop + document.documentElemetn.scrollTop;
+		}
+
+		x -= element.offsetLeft;
+		y -= element.offsetTop;
+
+		mouse.x = x;
+		mouse.y = y;
+	},false);
+	return mouse;
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
