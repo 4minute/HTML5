efaefe8ddcd25eae1d5c73cc1e88587cbe2d2885
yeting <yeting@broada.com>
2017-06-27 09:44:21 +0800

    chapter 8

--- Chapter 8/utils.js
+++ Chapter 8/utils.js
@@ -1,4 +1,6 @@
 var utils={}
+
+
 utils.colorToRGB = function (color,alpha){
 	if(typeof color === 'string' && color[0] ==='#'){
 		color = window.parseInt(color.slice(1),16);
@@ -17,7 +19,15 @@ utils.colorToRGB = function (color,alpha){
 	}
 };
 
+
 utils.parseColor = function(color,toNumber){
+	// if(!window.cancelRequestAnimationFrame){
+	// 	window.cancelRequestAnimationFrame = window.cancelAnimationFrame||
+	// 																			 window.webkitCancelRequestAnimationFrame||
+	// 																			 window.oCancelRequestAnimationFrame||
+	// 																			 window.msCancelRequestAnimationFrame||
+	// 																			 window.clearTimeout);
+	// }
 	if(toNumber === true){
 		if(typeof color === 'number'){
 			return (color|0);
@@ -32,6 +42,7 @@ utils.parseColor = function(color,toNumber){
 		}
 		return color;
 	}
+
 };
 
 
@@ -62,19 +73,35 @@ utils.containsPoint = function(rect,x,y){
 			 y < rect.y||y>rect.y+rect.height);
 };
 
+utils.captureTouch = function(element){
+	var touch = {x:null,y:null,isPressed:false};
 
+	element.addEventListener('touchstart',function(event){
+		touch.isPressed = true;
+	},false);
 
+	element.addEventListener('touchend',function(event){
+		touch.isPressed = false;
+		touch.x = null;
+		touch.y = null;
+	},false);
 
+	element.addEventListener('touchmove',function(){
+		var x,y,
+			touch_event = event.touches[0];
 
+		if(touch_event.pageX || touch_event.pageY){
+			x = touch_event.pageX;
+			y = touch_event.pageY;
+		}else{
+			x = touch_event.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
+			y = touch_event.clientY + document.body.scrollTop + document.documentElement.scrollTop;
+		}
+		x -= offsetLeft;
+		y -= offsetTop;
 
-
-
-
-
-
-
-
-
-
-
-
+		touch.x = x;
+		touch.y = y;
+	},false);
+	return touch;
+};
